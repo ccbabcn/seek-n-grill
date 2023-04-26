d1be404b16de35792b8308e3145de5712b082287
Cristian Bermudez Agudelo <cristian@Cristians-MacBook-Pro.local>
2023-04-27 00:50:24 +0200

    adds alternative approach to index spec

--- apps/web/specs/index.spec.tsx
+++ apps/web/specs/index.spec.tsx
@@ -1,4 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
 import Page from '../app/page'
+import { gql } from '../data-access/graphql-client'
+
+// //MOCK USERS FETCH
+gql.GetUsers = jest.fn().mockResolvedValue({ users: [] })
 
 /**
  * @param {function} Component
@@ -10,7 +16,13 @@ async function resolvedComponent({ Component, props = {} }) {
   return () => ComponentResolved
 }
 
-describe('Page component test', () => {
+describe('Index', () => {
+  it('should render successfully', async () => {
+    const { baseElement } = render((await Page()) as unknown as React.ReactElement)
+    expect(baseElement).toBeTruthy()
+  })
+})
+describe('Page component test alternative approach', () => {
   it('should render successfully the async Page component', async () => {
     const PageResolved = await resolvedComponent({ Component: Page })
     expect(PageResolved).toBeTruthy()
