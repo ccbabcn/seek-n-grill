ec680b9ebed2d97a8425ae0f1c1904a575e28233
Cristian Bermudez Agudelo <cristian@Cristians-MacBook-Pro.local>
2023-05-03 23:40:00 +0200

    fixes location resolvers and services initials tests

--- libs/api/feature-location/src/lib/location.resolver.spec.ts
+++ libs/api/feature-location/src/lib/location.resolver.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { LocationResolver } from './location.resolver'
 import { LocationService } from './location.service'
+import { PrismaService } from '@seek-n-grill/api/data-access-db'
 
 describe('LocationResolver', () => {
   let resolver: LocationResolver
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationResolver, LocationService]
+      providers: [LocationResolver, LocationService, PrismaService]
     }).compile()
 
     resolver = module.get<LocationResolver>(LocationResolver)
--- libs/api/feature-location/src/lib/location.service.spec.ts
+++ libs/api/feature-location/src/lib/location.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { LocationService } from './location.service'
+import { PrismaService } from '@seek-n-grill/api/data-access-db'
 
 describe('LocationService', () => {
   let service: LocationService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationService]
+      providers: [LocationService, PrismaService]
     }).compile()
 
     service = module.get<LocationService>(LocationService)
